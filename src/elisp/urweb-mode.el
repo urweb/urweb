dfafca5fcf01c61ed0183e4b80cdef90dfbfa822
Vladimir Shabanov <vshabanoff@gmail.com>
2011-09-26 02:54:52 +0400

    Minor tweak to speed up Emacs mode.

--- src/elisp/urweb-mode.el
+++ src/elisp/urweb-mode.el
@@ -185,7 +185,22 @@ See doc for the variable `urweb-mode-info'."
               (setq answer t)
               (setq finished t))))
          ((looking-at "</xml>")
-          (incf depth))))
+          (incf depth))
+
+         ((and (= depth 0)
+               (not (looking-at "<xml")) ;; ignore <xml/>
+               (eq font-lock-tag-face
+                   (get-text-property (point) 'face)))
+          ;; previous code was highlighted as tag, seems we are in xml
+          (progn
+            (setq answer t)
+            (setq finished t)))
+
+         ((= depth 0)
+          ;; previous thing was a tag like, but not tag
+          ;; seems we are in usual code or comment
+          (setq finished t))
+         ))
       answer)))
 
 (defun amAttribute (face)
