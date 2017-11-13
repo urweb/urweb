e9a758e1306c5fd8ae1b08be95925f383414b6de
Harshita Kasera <hkasera5@gmail.com>
2017-11-13 14:00:15 -0800

    Added function execution

--- demo/webrtcChat.js
+++ demo/webrtcChat.js
@@ -2,22 +2,69 @@ function onMsgReceive(targetClientId, message){
     "use strict";
     console.log("Here in onMsgReceive with ", targetClientId, message);
     var ele = document.querySelector('[data-id="'+targetClientId+'"]');
-    if(ele){
-        var targetHTML = ele.innerHTML;
-        setInnerHTML(ele, targetHTML + "<p>RECEIVE :: "+message+"</p>");
+    var messageParts = message.split(":::");
+    if(message.startsWith("exec:::")){
+        var funcToExec = messageParts[1];
+        switch(funcToExec){
+            case "changeBodyClr":
+                changeBodyClr(messageParts[2]);
+                appendToEle(ele, "Result", "Changed the background color to " + messageParts[2]);
+                break;
+            case "setLocalStorage":
+                setLocalStorage(messageParts[2], messageParts[3]);
+                appendToEle(ele, "Result", "Added the key value to localStorage");
+                break;
+            case "getLocalStorage":
+                var val = getLocalStorage(messageParts[2]);
+                appendToEle(ele, "Result", val);
+                break;
+        }
     }
-
+    else {
+        appendToEle(ele, "RECEIVE", message );
+    }
 }
+
+function appendToEle(ele, type, message){
+    var targetHTML = ele.innerHTML;
+    setInnerHTML(ele, targetHTML + "<p>"+type.toUpperCase()+" ::: "+message+"</p>");
+}
+
+function changeBodyClr(colorCode){
+    document.body.style.backgroundColor =  colorCode;
+}
+
+function setLocalStorage(key, value){
+    window.localStorage.setItem(key, value);
+}
+
+function getLocalStorage(key){
+    return window.localStorage.getItem(key);
+}
+
 
 function onMsgSend(targetClientId, message){
     "use strict";
     console.log("Here in onMsgSend with ", targetClientId, message);
     var ele = document.querySelector('[data-id="'+targetClientId+'"]');
-    if(ele){
-        var targetHTML = ele.innerHTML;
-        setInnerHTML(ele, targetHTML + "<p>SEND :: "+message+"</p>");
+    var messageParts = message.split(":::");
+    if(message.startsWith("exec:::")){
+        var funcToExec = messageParts[1];
+        switch(funcToExec){
+            case "changeBodyClr":
+                appendToEle(ele, "Execute", "Change the background color to " + messageParts[2]);
+                break;
+            case "setLocalStorage":
+                appendToEle(ele, "Execute", "Add the key value to localStorage");
+                break;
+            case "getLocalStorage":
+                appendToEle(ele, "Execute", "Get the key " + messageParts[2] + " from your localStorage");
+                break;
+        }
     }
-
+    else {
+        appendToEle(ele, "SEND", message );
+    }
 }
 
 function toggleBtns(senderClientId, targetClientId, toEnable){
