7bcf03e44bcb269281365b619a2333e06d5e0766
Patrick Mylund Nielsen <patrick@patrickmn.com>
2022-05-29 00:55:47 -0400

    Fix 'expected identifier or ( before numeric constant' error for true/false in GCC 11.2.0

--- src/c/urweb.c
+++ src/c/urweb.c
@@ -3119,13 +3119,13 @@ char *uw_Basis_sqlifyTimeN(uw_context ctx, uw_Basis_time *t) {
 }
 
 char *uw_Basis_ensqlBool(uw_Basis_bool b) {
-  static uw_Basis_int true = 1;
-  static uw_Basis_int false = 0;
+  static uw_Basis_int t = 1;
+  static uw_Basis_int f = 0;
 
   if (!b)
-    return (char *)&false;
+    return (char *)&f;
   else
-    return (char *)&true;
+    return (char *)&t;
 }
 
 uw_Basis_string uw_Basis_intToString(uw_context ctx, uw_Basis_int n) {
@@ -3223,13 +3223,13 @@ uw_Basis_char *uw_Basis_stringToChar(uw_context ctx, uw_Basis_string s) {
 
 uw_Basis_bool *uw_Basis_stringToBool(uw_context ctx, uw_Basis_string s) {
   (void)ctx;
-  static uw_Basis_bool true = uw_Basis_True;
-  static uw_Basis_bool false = uw_Basis_False;
+  static uw_Basis_bool t = uw_Basis_True;
+  static uw_Basis_bool f = uw_Basis_False;
 
   if (!strcasecmp (s, "True"))
-    return &true;
+    return &t;
   else if (!strcasecmp (s, "False"))
-    return &false;
+    return &f;
   else
     return NULL;
 }
